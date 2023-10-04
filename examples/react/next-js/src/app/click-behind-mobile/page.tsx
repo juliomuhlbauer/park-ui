67ff83afd1d7e9f9f7f10100c1d1b21064eb6e36
Júlio Mühlbauer <juliowernermm@gmail.com>
2023-10-04 10:07:47 -0300

    ClickBehindMobile

---
+++ examples/react/next-js/src/app/click-behind-mobile/page.tsx
@@ -0,0 +1,98 @@
+'use client'
+
+import { Portal } from '@ark-ui/react'
+import {
+  ChevronRightIcon,
+  CreditCardIcon,
+  LogOutIcon,
+  MailIcon,
+  MessageSquareIcon,
+  PlusCircleIcon,
+  SettingsIcon,
+  UserIcon,
+  UserPlusIcon,
+} from 'lucide-react'
+import { HStack, Stack } from 'styled-system/jsx'
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
+import {
+  Menu,
+  MenuContent,
+  MenuItem,
+  MenuItemGroup,
+  MenuItemGroupLabel,
+  MenuPositioner,
+  MenuSeparator,
+  MenuTrigger,
+  MenuTriggerItem,
+  type MenuProps,
+} from '~/components/ui/menu'
+import { Typography } from '~/components/ui/typography'
+
+export default function ClickBehindMobile() {
+  return (
+    <Stack>
+      <DemoMenu />
+
+      {Array.from({ length: 10 }).map((_, i) => (
+        <Input key={i} placeholder={`Input ${i}`} />
+      ))}
+    </Stack>
+  )
+}
+
+export const DemoMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuTrigger asChild>
+        <Button variant="outline" size={props.size}>
+          Open menu
+        </Button>
+      </MenuTrigger>
+      <Portal>
+        <MenuPositioner>
+          <MenuContent>
+            <MenuItemGroup id="group-1">
+              <MenuItemGroupLabel htmlFor="group-1">My Account</MenuItemGroupLabel>
+              <MenuSeparator />
+              <MenuItem id="profile">
+                <HStack gap="6" justify="space-between" flex="1">
+                  <HStack gap="2">
+                    <UserIcon />
+                    Profile
+                  </HStack>
+                  <Typography as="span" color="fg.subtle" textStyle="xs">
+                    ⇧⌘P
+                  </Typography>
+                </HStack>
+              </MenuItem>
+              <MenuItem id="billing">
+                <HStack gap="2">
+                  <CreditCardIcon /> Billing
+                </HStack>
+              </MenuItem>
+              <MenuItem id="settings">
+                <HStack gap="6" justify="space-between" flex="1">
+                  <HStack gap="2">
+                    <SettingsIcon /> Settings
+                  </HStack>
+                  <Typography as="span" color="fg.subtle" textStyle="xs">
+                    ⌘,
+                  </Typography>
+                </HStack>
+              </MenuItem>
+
+              <MenuSeparator />
+              <MenuItem id="logout">
+                <HStack gap="2">
+                  <LogOutIcon />
+                  Logout
+                </HStack>
+              </MenuItem>
+            </MenuItemGroup>
+          </MenuContent>
+        </MenuPositioner>
+      </Portal>
+    </Menu>
+  )
+}
